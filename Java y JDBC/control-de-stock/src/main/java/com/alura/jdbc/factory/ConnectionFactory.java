c8383f946619ec840f0540dcfeae24391fa4911b
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-02-13 22:39:22 -0300

    Aula 04(66%) completada

--- Java y JDBC/control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ Java y JDBC/control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,13 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+
 public class ConnectionFactory {
 	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("@Mac40957026");
+		pooledDataSource.setMaxPoolSize(10);
+		
+		this.datasource = pooledDataSource;
+	}
+	
 	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                "@Mac40957026");
+		return this.datasource.getConnection();
 	}
 
 }
---
+++ Java y JDBC/control-de-stock/src/main/java/com/alura/jdbc/pruebas/PruebaPoolDeConexiones.java
@@ -0,0 +1,21 @@
+package com.alura.jdbc.pruebas;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.alura.jdbc.factory.ConnectionFactory;
+
+public class PruebaPoolDeConexiones {
+	
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory cf = new ConnectionFactory();
+		
+		for (int i = 0 ; i < 20 ; i++) {
+			Connection con = cf.recuperaConexion();
+			
+			System.out.println("Abriendo la conexión de número " + (i+1));
+		}
+		
+	}
+
+}
